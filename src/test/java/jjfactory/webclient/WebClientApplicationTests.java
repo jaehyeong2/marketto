952451c927a2f06f2fd547bf0e15e6c69a2292f2
jae <wogud1514@naver.com>
2022-10-08 15:45:50 +0900

    세팅

---
+++ src/test/java/jjfactory/webclient/WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package jjfactory.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
