6d0c77545b6e7ed8b880f1e8663e65b373d25d47
jae <wogud1514@naver.com>
2022-10-11 21:09:13 +0900

    feat : global dto들 추가

---
+++ src/main/java/jjfactory/webclient/global/dto/req/MyPageReq.java
@@ -0,0 +1,22 @@
+package jjfactory.webclient.global.dto.req;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+
+@NoArgsConstructor
+@Getter
+public class MyPageReq {
+    private int page;
+    private int size;
+
+    public PageRequest of(){
+        return PageRequest.of(page-1,size);
+    }
+
+    public MyPageReq(int page, int size) {
+        this.page = page;
+        this.size = size;
+    }
+}
---
+++ src/main/java/jjfactory/webclient/global/dto/res/PagingRes.java
@@ -0,0 +1,31 @@
+package jjfactory.webclient.global.dto.res;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+public class PagingRes<T> {
+    private int currentPage;
+    private int totalPage;
+    private long totalCount;
+    private List<T> resultList;
+
+    public PagingRes(int currentPage, int totalPage, long totalCount, List<T> resultList) {
+        this.currentPage = currentPage;
+        this.totalPage = totalPage;
+        this.totalCount = totalCount;
+        this.resultList = resultList;
+    }
+
+    public PagingRes(Page<T> page) {
+        this.currentPage = page.getPageable().getPageNumber()+1;
+        this.totalPage = page.getTotalPages();
+        this.totalCount = page.getTotalElements();
+        this.resultList = page.getContent();
+    }
+}
